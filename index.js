76132db8d71fd46e2bc11cfb3d0c64fc77f0d75e
rod773 <104315743+rod773@users.noreply.github.com>
2024-01-15 09:54:35 +0100

    update

--- index.js
+++ index.js
@@ -10,25 +10,28 @@ const PORT = process.env.PORT || 8000;
 
 const app = express();
 
+app.use(express.json());
+
 const server = createServer(app);
 
 const io = new Server(server);
 
-app.get("/test", (req, res) => {
-  res.send("this is a test");
+app.get("/test", async (req, res) => {
+  const inference = new HfInference(process.env.TOKEN); // your user token
+
+  const gpt2 = inference.endpoint(
+    "https://api-inference.huggingface.co/models/OpenAssistant/oasst-sft-4-pythia-12b-epoch-3.5"
+  );
+  const { generated_text } = await gpt2.textGeneration({
+    inputs: "The answer to the universe is",
+  });
+
+  res.send({
+    input: "The answer to the universe is",
+    generated_text: generated_text,
+  });
 });
 
 server.listen(PORT, () => {
   console.log(`http://localhost:${PORT}/test`);
 });
-
-const inference = new HfInference(process.env.TOKEN); // your user token
-
-const gpt2 = inference.endpoint(
-  "https://api-inference.huggingface.co/models/OpenAssistant/oasst-sft-4-pythia-12b-epoch-3.5"
-);
-const { generated_text } = await gpt2.textGeneration({
-  inputs: "The answer to the universe is",
-});
-
-console.log(generated_text);
